ff1eea456f4b0b82726dece23906cca15f0dfe88
walkr <walkr@local>
2019-03-06 20:29:11 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EnvHelper.Mixfile do
   def project do
     [
       app: :env_helper,
-      version: "0.0.6",
+      version: "0.1.0",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
