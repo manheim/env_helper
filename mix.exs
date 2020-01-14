b8aacdd8e97a2641f26a16462243b2d9a12ed066
Paul Daigle and Shannon Brooks <vertex-team+paul+shannon@coxautoinc.com>
2020-01-14 19:12:24 +0000

    Update version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EnvHelper.Mixfile do
   def project do
     [
       app: :env_helper,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
